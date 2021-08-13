6ea6c9b4b22cd63da7d8eaddf2ef5813ac7015b8
pelenthium <pelenthium@gmail.com>
2021-08-13 18:47:42 +0800

    Task 3: Purchase domain and methods + some utils

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"github.com/ozonva/ova-purchase-api/internal/purchase"
 	"math"
 )
 
@@ -25,6 +26,35 @@ func Split(input []int, size int) ([][]int, error) {
 		buf := make([]int, len(source))
 		copy(buf, source)
 		result = append(result, buf)
+	}
+	return result, nil
+}
+
+func SplitToBulks(input []purchase.Purchase, size uint) ([][]purchase.Purchase, error) {
+	if size <= 0 {
+		return nil, errors.New("size must be positive")
+	}
+	batches := int(math.Ceil(float64(len(input)) / float64(size)))
+	result := make([][]purchase.Purchase, 0, batches)
+	isize := int(size)
+	for i := 0; i < batches; i++ {
+		left := i * isize
+		right := min(len(input), left+isize)
+		source := input[left:right]
+		buf := make([]purchase.Purchase, len(source))
+		copy(buf, source)
+		result = append(result, buf)
+	}
+	return result, nil
+}
+
+func ToMap(input []purchase.Purchase) (map[uint64]purchase.Purchase, error) {
+	result := make(map[uint64]purchase.Purchase, len(input))
+	for _, v := range input {
+		if _, ok := result[v.Id]; ok {
+			return nil, errors.New("two purchase with the same id")
+		}
+		result[v.Id] = v
 	}
 	return result, nil
 }
